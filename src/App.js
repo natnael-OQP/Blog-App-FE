ee0cabacb3fc0bdc532de9a86b99061923362890
papa-react <makiya112545@gmail.com>
2022-03-09 12:14:56 +0300

    use dynamic page rout

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 				<Route path="/" element={user ? <Homepage /> : <Register />} />
 				<Route path="/posts" element={<Homepage />} />
 				<Route path="/login" element={user ? <Homepage /> : <Login />} />
-				<Route path="/post:id" element={<Single />} />
+				<Route path="/post/:id" element={<Single />} />
 				<Route path="/write" element={user ? <Write /> : <Login />} />
 				<Route path="/settings" element={user ? <Settings /> : <Login />} />
 			</Routes>
