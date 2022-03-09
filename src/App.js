4fc9df2c511a2078cf8e76e17960ed66e839e414
papa-react <makiya112545@gmail.com>
2022-03-09 08:36:25 +0300

    create register and login pages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
 import Header from "./components/header/Header";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 function App() {
+	const user = false;
 	return (
 		<Router>
 			<Header />
 			<Routes>
 				<Route path="/" element={<Homepage />} />
+				{user ? (
+					<Route path="/" element={<Homepage />} />
+				) : (
+					<Route path="/register" element={<Register />} />
+				)}
+				<Route path="/posts" element={<Homepage />} />
+				<Route path="/login" element={<Login />} />
 			</Routes>
 		</Router>
 	);
